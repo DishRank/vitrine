beff46bb1f0232a1f977435226dd271c7f2b0050
Gregoire63 <gregoire.raturat@laposte.net>
2026-03-28 20:50:38 +0100

    fix request supabase

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ const securityHeaders = [
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' https: data:",
-      "connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com",
+      "connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://yztbhdvrvgozhyaujtjz.supabase.co",
       "frame-ancestors 'none'",
       "base-uri 'self'",
       "form-action 'self'",
